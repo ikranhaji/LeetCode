78772caed4b6db0d1a4c2cbd698cfede0bad22be
ikranhaji <121581855+ikranhaji@users.noreply.github.com>
2023-11-20 12:20:28 -0600

    Time: 478 ms (53.89%), Space: 13.3 MB (58.28%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        nums.sort() 
+        closestSum = float('inf')  
+
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                currentSum = nums[i] + nums[left] + nums[right]
+
+                if abs(target - currentSum) < abs(target - closestSum):
+                    closestSum = currentSum
+
+                if currentSum < target:
+                    left += 1
+                elif currentSum > target:
+                    right -= 1
+                else:
+                    return closestSum  
+
+        return closestSum  
