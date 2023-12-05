e70e7a1fb730d68b8a7f4b3825ff7563980e985d
ikranhaji <121581855+ikranhaji@users.noreply.github.com>
2023-12-05 06:26:18 -0600

    Time: 18 ms (41.70%), Space: 13.4 MB (18.88%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += 1
+            if digits[i] < 10:
+                return digits
+            digits[i] = 0
+        digits.insert(0, 1)
+
+        return digits
